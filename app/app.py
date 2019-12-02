cbd0bc52235e6f62b389e465ae2e0ba948da77dd
Chris Abrami <57453141+abramic@users.noreply.github.com>
2019-12-02 09:47:46 -0800

    Basic flask server set up with ping functionality

--- app/app.py
+++ app/app.py
@@ -1,1 +1,8 @@
-# Hello World!
+from flask import Flask, request, make_response, jsonify
+app = Flask("python_email_test_server")
+
+@app.route('/ping', methods = ['GET', 'PATCH', 'POST', 'PUT', 'DELETE'])
+def handler():
+    return make_response('Hello World From Python Test Email Server!', 200)
+
+app.run(port=int('5001'), debug=True)
